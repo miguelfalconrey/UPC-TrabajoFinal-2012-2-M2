dbb9075cd92e33f299631a00b3b88c62b9369437
Miguel Falcon Rey <miguelfalconrey@hotmail.com>
2012-11-18 02:16:49 -0500

    Creacion estructura

---
+++ Crm/src/crm/Crm.java
@@ -0,0 +1,14 @@
+
+package crm;
+
+
+public class Crm {
+
+    
+    public static void main(String[] args) {
+        
+     System.out.println("Mensaje de prueba");
+      
+            
+    }
+}
