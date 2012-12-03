eea8fb034e43fa174447576156b818ac72259d79
Miguel Falcon Rey <miguelfalconrey@hotmail.com>
2012-12-02 21:04:14 -0500

    Update TDD Autenticacion Usuarios
    Autenticacion de usuarios, Buscar, Eliminarr

--- Crm/src/crm/AppUsuario.java
+++ Crm/src/crm/AppUsuario.java
@@ -11,39 +11,88 @@ import java.util.ArrayList;
  * @author gerald
  */
 public class AppUsuario {
-    
+
     ArrayList<Usuario> usuarios; //creacion de array de usuarios
 
     public AppUsuario() {
         usuarios = new ArrayList<Usuario>(); /* instanciando un array de objetos*/
     }
-    
-    
-    
-    public boolean RegistrarUsuario(String dni, String nombre, String apellidopaterno, String apellidomaterno, 
+
+    public boolean RegistrarUsuario(String dni, String nombre, String apellidopaterno, String apellidomaterno,
             String usuario, String correo, String fechaingreso, String cargo, String rol, String contrasena,
             String creadopor, String fechadecreacion, String actualizadopor, String fechadeactualizacion)
-            throws CRM_Exception{ //procedimiento publico en donde le damos los parametros
-        
+            throws CRM_Exception { //procedimiento publico en donde le damos los parametros
+
         boolean rpta = false;
-        
+
         rpta = usuarios.add(new Usuario(dni, nombre, apellidopaterno, apellidomaterno, usuario, correo, fechaingreso,
                 cargo, rol, contrasena, creadopor, fechadecreacion, actualizadopor, fechadeactualizacion));
         //creo un nuevo objeto de clase de Usuario
-        
+
         return rpta;
-        
+
     }
-    
-    
-    public Usuario BuscarUsuario(String nombre){
-        for (Usuario usu:usuarios){
-            if(usu.getUsuario().trim().equals(nombre)){
+
+    public Usuario BuscarUsuario(String nombre) {
+        for (Usuario usu : usuarios) {
+            if (usu.getUsuario().trim().equals(nombre)) {
                 return usu;
             }
         }
         return null;
-        
+
     }
-    
+
+    public void eliminarusuario(String usuario) {
+        Usuario usu = BuscarUsuario(usuario);
+        usuarios.remove(usu);
+
+
+        usuarios.indexOf(usu);
+
+    }
+
+    public void editarusuario(String dni, String nombre, String apellidopaterno,
+            String apellidomaterno, String usuario, String correo, String fechaingreso,
+            String cargo, String rol, String contrasena, String creadopor,
+            String fechadecreacion, String actualizadopor, String fechadeactualizacion) {
+
+        int index; //Para guardar el indice
+
+        Usuario usu = BuscarUsuario(usuario);
+        usu.setCargo(cargo);
+        usu.setRol(rol);
+        usu.setActualizadopor(actualizadopor);
+
+        index = usuarios.indexOf(usu);
+        usuarios.set(index, usu);
+    }
+
+    public boolean validarusuariocontrasena(String usuario, String contrasena) throws CRM_Exception {
+        boolean rpta = false;
+        Usuario usu = BuscarUsuario(usuario);
+
+        if (usu == null) {
+            String mensaje = "Usuario o contraseña incorrecta";
+            if (!mensaje.isEmpty()) {
+                throw new CRM_Exception(mensaje);
+            }
+            rpta = false;
+        } else {
+
+            if (usu.getContrasena().trim().equals(contrasena)) {
+                rpta = true;
+
+            } else {
+                String mensaje = "Usuario o contraseña incorrecta";
+                if (!mensaje.isEmpty()) {
+                    throw new CRM_Exception(mensaje);
+                }
+                rpta = false;
+            }
+        }
+
+        return rpta;
+
+    }
 }
--- Crm/test/crm/AppUsuarioTest.java
+++ Crm/test/crm/AppUsuarioTest.java
@@ -11,46 +11,99 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import static org.junit.Assert.*;
 
-
 /**
  *
  * @author gerald
  */
 public class AppUsuarioTest {
-    
+
+    AppUsuario usu = new AppUsuario();
     //creacion de usuario
+
     @Test
     public void testRegistrarUsuario() throws Exception {
-        String dni = "45263659";
-        String nombre = "Juan";
-        String apellidopaterno = "Perez";
-        String apellidomaterno = "Loza";
-        String usuario = "u201212";
-        String correo = "u201212@gmail.com";
-        String fechaingreso = "21/10/2011";
-        String cargo = "Tecnico";
-        String rol = "soporte";
-        String contrasena = "12345678";
-        String creadopor = "Carlos";
-        String fechadecreacion = "21/10/2011";
-        String actualizadopor = "";
-        String fechadeactualizacion = "";
-        
+        String[] dni = {"45", "34"};
+        String[] nombre = {"Juan", "Carlos"};
+        String[] apellidopaterno = {"Perez", "Lopez"};
+        String[] apellidomaterno = {"Loza", "Giraldo"};
+        String[] usuario = {"u201212", "u2012333"};
+        String[] correo = {"u201212@gmail.com", "u2012333@hotmail.com"};
+        String[] fechaingreso = {"21/10/2011", "21/12/2012"};
+        String[] cargo = {"Tecnico", "Administrador"};
+        String[] rol = {"soporte", "Service"};
+        String[] contrasena = {"12345678", "654321"};
+        String[] creadopor = {"Carlos", "Adminweb"};
+        String[] fechadecreacion = {"21/10/2011", "22/12/2012"};
+        String[] actualizadopor = {"", ""};
+        String[] fechadeactualizacion = {"", ""};
+
         boolean rpta = false;
-        
-        AppUsuario usu = new AppUsuario();
-        
-         rpta=usu.RegistrarUsuario(dni, nombre, apellidopaterno, apellidomaterno, usuario, correo, fechaingreso, cargo, rol,
-                contrasena, creadopor, fechadecreacion, actualizadopor, fechadeactualizacion);
-                
-        Usuario nuevo = usu.BuscarUsuario(usuario);
-        
-        //crea el usuario
-        
-        
-        System.out.println(nuevo.getUsuario());
-        
+
+        for (int i = 0; i < dni.length; i++) {
+
+            rpta = usu.RegistrarUsuario(dni[i], nombre[i], apellidopaterno[i], apellidomaterno[i], usuario[i], correo[i], fechaingreso[i], cargo[i], rol[i],
+                    contrasena[i], creadopor[i], fechadecreacion[i], actualizadopor[i], fechadeactualizacion[i]);
+        }
+
+        Usuario nuevo = usu.BuscarUsuario("u2012333");
+        //crea el usuario        
+
         assertEquals(true, rpta);
     }
+
+    @Test
+    public void testEliminarUsuario() throws Exception {
+        testRegistrarUsuario(); //Llamando al primer test
+
+        String usuario = "u2012333";
+        usu.eliminarusuario(usuario);
+        //usu.eliminarusuario("u2012333d"); //Generar error
+        Usuario nuevo = usu.BuscarUsuario(usuario);
+        assertNull("El usuario " + usuario + " no existe.", nuevo);
+    }
+    
+    @Test
+    public void testModificarUsuario() throws Exception {
+        testRegistrarUsuario(); //Llamando al primer test
+
+        String dni = "34";
+        String nombre = "Carlos";
+        String apellidopaterno = "Lopez";
+        String apellidomaterno = "Giraldo";
+        String usuario = "u2012333";
+        String correo = "u2012333@hotmail.com";
+        String fechaingreso = "21/12/2012";
+        String cargo = "Helpdesk";
+        String rol =  "Super";
+        String contrasena = "654321";
+        String creadopor = "Adminweb";
+        String fechadecreacion = "22/12/2012";
+        String actualizadopor = "Miguel";
+        String fechadeactualizacion = "24/12/2012";
+        
+        usu.editarusuario(dni, nombre, apellidopaterno, apellidomaterno, usuario,
+                correo, fechaingreso, cargo, rol,
+                    contrasena, creadopor, fechadecreacion, actualizadopor, fechadeactualizacion);
+        //usu.eliminarusuario("u2012333d"); //Generar error
+        Usuario editar = usu.BuscarUsuario(usuario);
+        assertEquals(cargo,editar.getCargo());
+        assertEquals(rol,editar.getRol());
+        assertEquals(actualizadopor,editar.getActualizadopor());
+    
+    }
+
+    @Test
+    public void testValidarUsuarioContrasena() throws Exception {
+    testRegistrarUsuario();
+    
+    String usuario="u2012333";
+    String contrasena="654321";
+    boolean rpta = false;
+    
+    rpta=usu.validarusuariocontrasena(usuario,contrasena);
+    assertEquals(true, rpta);
+            
+    }
+    
     
 }
